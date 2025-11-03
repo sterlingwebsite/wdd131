b4be6caaa95aa1123eb327521466dd2a5d583eb3
sterlingwebsite <ssteele1@byupathway.edu>
2025-11-03 10:44:38 -0700

    W02 Learning Activity: Handling DOM Events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,31 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        // attributes, properties
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+        // removes the li element when clicked
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        // Appended children
+        li.append(deleteButton);
+        list.append(li);
+
+        // clears the input value
+        input.value = '';
+
+        // focus (active cursor) sent to the input element
+        input.focus();
+    }
+});
